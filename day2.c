b178f9bac8ae074d54dcbe611339682207de0d18
Yash Lokare <yashlokare0106@gmail.com>
2026-05-27 14:00:58 +0530

    scanf input program done

---
+++ day2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    int age;
+
+    printf("my name:");
+    scanf("%s", name);
+    
+    printf("my age :");
+    scanf("%d", &age);
+
+    printf("name: %s\n", name);
+    printf("age: %d\n", age);
+    printf("Day 2 bhi completed \n");
+    return 0;
+}
